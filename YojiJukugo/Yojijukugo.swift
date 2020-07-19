689d63ecfa443673fcbcb4a569c2cf9be2468c5c
Yukimasa Kuwabara <yukkin1204.mathematics@gmail.com>
2020-07-19 22:00:39 +0900

    Yojijukugoモデルを作成

---
+++ YojiJukugo/Yojijukugo.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Yojijukugo{
+    let character: String
+    let reading: String
+    let meaning: String
+    
+    init(character: String, reading: String, meaning: String){
+        self.character = character
+        self.reading = reading
+        self.meaning = meaning
+    }
+}
